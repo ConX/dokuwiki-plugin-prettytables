8f3c27515324ee4f7f9d7df746cab6d5f54b11fe
Constantinos Xanthopoulos <conx@xanthopoulos.info>
2012-09-04 04:20:40 +0300

    OOP design, renaming global scope function & fixing parsing

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /**
  * String manipulation function similar to 
- * Ruby's and pythons "repeat" build in functions.
+ * Ruby's and pythons "prettytables_repeat" build in functions.
  * @author szsk (http://snipplr.com/users/szsk/)
  **/
-function repeat(text,num) {
+function prettytables_repeat(text,num) {
 	for( var i = 0, buf = ""; i < num; i++ ){
 		buf += text;
 	}
@@ -15,112 +15,131 @@ function repeat(text,num) {
  * Ruby's and pythons "center" build in functions.
  * @author szsk (http://snipplr.com/users/szsk/)
  **/
-function strcenter (text, width) {
+function prettytables_strcenter (text, width) {
 	padding = " ";
 	if( text.length < width ) { 
 		var len = width - text.length;
 		var remain = ( len % 2 === 0 ) ? "" : padding;
-		var pads = repeat(padding, parseInt(len / 2, 10));
+		var pads = prettytables_repeat(padding, parseInt(len / 2, 10));
 		return pads + text + pads + remain;
 	}   
 	else{
-
 		return text;
 	}
 }
 
-/**
- * Helper function that creates a map for the position
- * of headers (^) in the tables.
- * @author ConX <conx@xanthopoulos.info>
- **/
-function mapHeaders(text){
-	var row = [];
-	for (var i=0; i<text.length; i++){
-		text = text.replace(/\[\[.*\|.*\]\]/g, "");
-		if (text.charAt(i) == '^'){
-			row.push(1);
-		}
-		else if(text.charAt(i) == "|"){
-			row.push(0);
-		}
-	}
-	return row;
-}
 
-/**
- * The actual reformating function for the plugin
- * @param string sample		Table's source code
+/** Table representation object.
  * @author Constantinos Xanthopoulos <conx@xanthopoulos.info>
- **/
-function fixTableFormat(text) {
-	var lines =  text.split(/\n/);
-	var table = [];
-	var r = "";
-	var map = [];
-	var startre = /^[\^|]/;
-	var linkre = /^[^\[|]*[\[]{2}[^\]]*[\]]{2}[^|\^]*/;
-	var smileyre = /^[^\:]*[\:][\-][|][^|\^]*/;
-	var regre = /^[^|\[\^]+/;
-	for (var i=0;i<lines.length;i++){
-		if (lines[i] !== ""){
-			var c =[];
-			var t = lines[i];
-			var s;
-			map.push(mapHeaders(lines[i]));
-			if (t.match(startre)){
-				t = t.substr(1);
-				while (t.length != '0'){
-					if ((s = t.match(linkre)) !== null){
-						t = t.substr(s[0].length+1);
+**/
+var prettytable = {
+	table: [],
+	map:  [],
+
+	/**
+	 * Table parser function
+	 *
+	 **/
+	parse: function(text){
+		this.map = [];
+		this.table = [];
+		
+		var lines =  text.split(/\n/);
+		for (var i=0;i<lines.length;i++){
+			if (lines[i] !== ""){
+				var c =[];
+				var mr = [];
+				var t = this.encode(lines[i]);
+				var s;
+				if (t.match(/^[\^|]/)){
+					while (t.length != '0'){
+						s = t.match(/^[\^|]+/);
+						mr.push(s[0]);
+						t = t.substr(s[0].length);
+						t = t.replace(/^[\n ]+/,"");
+						if (t.length != 0){
+							s = t.match(/^([^|\^]+)[\^|]/);
+							t = t.substr(s[1].length);
+							s = s[1].replace(/^[ ]+/,"");
+							s = s.replace(/[ ]+$/,"");
+							c.push(s);
+						}
+						
 					}
-					else if ((s = t.match(smileyre)) !== null){
-						t = t.substr(s[0].length+1);
-					}
-					else{
-						s = t.match(regre);
-						t = t.substr(s[0].length+1);
-					}
-					s = s[0].replace(/^[ ]+/,"");
-					s = s.replace(/[ ]+$/,"");
-					c.push(s);
+					this.map.push(mr);
+					this.table.push(c);    
 				}
-				table.push(c);    
-			}
-			else{
-				alert("Not a table");
-				return null;
-			}
-		}            
-	}
-	var colsize = new Array(table[0].length);
-	for (i=0;i<colsize.length;i++){
-		colsize[i] = 0;
-	}
+				else{
+					alert("Not a table");
+					return(null);
+				}
+			}            
+		}
+		return(this);
+	},
 
-	for (i=0;i<table.length;i++){
-		for (j=0;j<table[i].length;j++){
-			if (table[i][j].length > colsize[j]){
-				colsize[j] = table[i][j].length;    
+	/**
+	 * Helper function that replaces certain non table uses of
+	 * the ^ and | symbols.
+	 * @author Constantinos Xanthopoulos <conx@xanthopoulos.info>
+	 **/
+	encode: function(line){
+		line = line.replace(/[%]{2}[|][%]{2}/g, "%%!@#1#@!%%");
+		line = line.replace(/[%]{2}[^][%]{2}/g, "%%!@#2#@!%%");
+		line = line.replace(/([\[]{2}.*)[|](.*[\]]{2})/g, "$1!@#1#@!$2");
+		return line
+	},
+
+	/** Invert the replacements made by encode.
+	 * @author Constantinos Xanthopoulos <conx@xanthopoulos.info>
+	 *
+	 **/
+	decode: function(){
+		for (var i=0;i<this.table.length;i++){
+			for (var j=0;j<this.table[i].length;j++){
+				t = this.table[i][j].replace(/[!][@][#][1][#][@][!]/g,"|");
+				this.table[i][j] = t.replace(/[!][@][#][2][#][@][!]/g,"^");
 			}
 		}
-	}
+	},
 
-	for (i=0;i<table.length;i++){
-		for (j=0;j<table[i].length;j++){
-			if (map[i][j] == 1){
-				r = r + "^";
+	/**
+	 * Generate formated table
+	 * @author ConX <conx@xanthopoulos.info>
+         **/
+	generate: function(){
+		var r = "";
+		var colsize = new Array(this.table[0].length);
+		this.decode();
+		for (i=0;i<colsize.length;i++){
+			colsize[i] = 0;
+		}
+
+		for (i=0;i<this.table.length;i++){
+			for (j=0;j<this.table[i].length;j++){
+				if (this.table[i][j].length > colsize[j]){
+					colsize[j] = this.table[i][j].length;    
+				}
+			}
+		}
+
+		for (i=0;i<this.table.length;i++){
+			for (j=0;j<this.table[i].length;j++){
+				r = r + this.map[i][j];
+				r = r + prettytables_strcenter(this.table[i][j],colsize[j]+4);
+			}
+			if (i < this.table.length-1){
+				r = r + this.map[i][j] +"\n";
 			}
 			else{
-				r = r + "|"; 
-			}        
-			r = r + strcenter(table[i][j],colsize[j]+4);
-		}
-		r = r + "|\n";
-	}
-	return r;
+				r = r + this.map[i][j];
+			}
 
-}
+		}
+		return r;
+	}
+};
+
 
 /**
  * Button action for prettytables
@@ -133,15 +152,15 @@ function addBtnActionPrettytables($btn, props, edid, id){
 			if(selection.getLength()){
 				var sample = fixtxt(selection.getText());
 				opts = {nosel: true};
-				sample = fixTableFormat(sample);
-				if (sample !== null){
-					pasteText(selection,sample,opts);
+				pt = Object.create(prettytable).parse(sample);
+				if (pt !== null){
+					pasteText(selection,pt.generate(),opts);
 				}
 			}
 			else{
 				alert("No selection");
 			}
 
-			});
+		});
 	return true;
 }
